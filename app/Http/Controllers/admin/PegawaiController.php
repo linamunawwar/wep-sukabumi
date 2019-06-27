4b2b384d1acf56e2401fb68420ba296717db8634
DESKTOP-NRTLCSN\Asus <nuramalina2911.na@gmail.com>
2019-06-27 14:08:04 +0700

    EDIT CV

--- app/Http/Controllers/admin/PegawaiController.php
+++ app/Http/Controllers/admin/PegawaiController.php
@@ -294,8 +294,23 @@ class PegawaiController extends Controller
        $bank['jiwasraya'] = $data['jiwasraya'];
        $bank['asuransi_lain'] = $data['nama_asuransi'];
        $bank['nomor_lain'] = $data['nomor_asuransi'];
-
-       $update_bank = BankAsuransi::where('nip',$data['nip'])->update($bank);
+       $find_bank = BankAsuransi::where('nip',$data['nip'])->first()
+       if($find_bank){
+        $update_bank = BankAsuransi::where('nip',$data['nip'])->update($bank);
+      }else{
+        $bank = new BankAsuransi;
+        $bank->nip = $data['nip'];
+         $bank->nama_bank = $data['nama_bank'];
+         $bank->no_rekening = $data['no_rek'];
+         $bank->npwp = $data['npwp'];
+         $bank->jamsostek = $data['jamsostek'];
+         $bank->dplk = $data['dplk'];
+         $bank->jiwasraya = $data['jiwasraya'];
+         $bank->asuransi_lain = $data['nama_asuransi'];
+         $bank->nomor_lain = $data['nomor_asuransi'];
+       
+        $bank->save();
+      }
 
        //-------------Pendidikan---------
         $del_pendidikan = Pendidikan::where('nip',$data['nip'])->delete();
