d6c2d77df1466cb2e5dc2e09213b80cf8660cc29
DESKTOP-NRTLCSN\Asus <nuramalina2911.na@gmail.com>
2019-10-03 13:56:46 +0700

    perubahan struktur pegawai

--- resources/views/layouts/blank.blade.php
+++ resources/views/layouts/blank.blade.php
@@ -9,7 +9,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="{{asset('public/img/Waskita.png')}}" rel='icon' type='image/x-icon'/>
-        <title>WEP - Becakayu 2A </title>
+        <title>WEP - Becakayu 2A Ujung</title>
 
         <!-- Bootstrap -->
         <link href="{{ asset("public/css/bootstrap.min.css") }}" rel="stylesheet">
