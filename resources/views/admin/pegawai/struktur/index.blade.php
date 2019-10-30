f739433ff4249ecb42e34446535f5a7f1e5c7080
DESKTOP-NRTLCSN\Asus <nuramalina2911.na@gmail.com>
2019-10-30 14:19:24 +0700

    Bismillah chart

--- resources/views/admin/pegawai/struktur/index.blade.php
+++ resources/views/admin/pegawai/struktur/index.blade.php
@@ -4,6 +4,7 @@
     <!-- Example -->
     <!--<link href=" <link href="{{ asset("css/myFile.min.css") }}" rel="stylesheet">" rel="stylesheet">-->
     <style type="text/css">
+
       #chart-container { background-color: #eee; }
       .orgchart { background: #fff; padding: 0px; }
       .orgchart .node .title { background-color: #006699; width: 100%; height: 100%; white-space: initial;}
@@ -50,9 +51,10 @@
             <div class="clearfix"></div>
           </div>
           <div class="x_content">
-            <ul id="ul-data" style="display: none;" >
+            <ul id="ul-data2" class="tree" >
               @foreach($posisi as $pos)
               <li id="{{$pos->id}}" title="{{$pos->posisi}}">
+                <b>{{$pos->posisi}}</b><br>
                 @foreach($pos->anggota as $anggota)
                   {{$anggota->nama}}
                 @endforeach
@@ -60,6 +62,7 @@
                     <ul>
                       @foreach($pos->anak as $anak)
                         <li id="{{$anak->id}}" title="{{$anak->posisi}}">
+                          <b>{{$anak->posisi}}</b><br>
                           @foreach($anak->anggota as $anggota1)
                             {{$anggota1->nama}}<br>
                           @endforeach
@@ -67,18 +70,17 @@
                               <ul>
                                 @foreach($anak->anak as $anak2)
                                     <li id="{{$anak->id}}" title="{{$anak2->posisi}}">
+                                      <b>{{$anak2->posisi}}</b><br>
                                     @foreach($anak2->anggota as $anggota2)
-                                      <li id="{{$anggota2->id}}" title="{{$anak2->posisi}}">
-                                        {{$anggota2->nama}}
-                                      </li>
+                                        {{$anggota2->nama}}<br>
                                     @endforeach
                                     @if(count($anak2->anak) != 0)
                                       <ul>
                                         @foreach($anak2->anak as $anak3)
+                                          <li id="{{$anak3->id}}" title="{{$anak3->posisi}}" >
+                                            <b>{{$anak3->posisi}}</b><br>
                                             @foreach($anak3->anggota as $anggota3)
-                                                <li id="{{$anggota3->id}}" title="{{$anak3->posisi}}" >
-                                                  {!!$anggota3->nama!!}
-                                                </li>                         
+                                                  {!!$anggota3->nama!!} <br>                      
                                             @endforeach
                                             @if(count($anak3->anak) != 0)
                                               <ul>
@@ -87,6 +89,60 @@
                                                 @endforeach
                                               </ul>
                                             @else
+                                          </li>
+                                            @endif
+                                        @endforeach
+                                      </ul>
+                                    @else
+                                  </li>
+                                    @endif
+                                @endforeach
+                              </ul>
+                            @else
+                              </li>
+                            @endif
+                      @endforeach
+                    </ul>
+                @else
+                  </li>
+                @endif
+              @endforeach
+            </ul>
+            <ul id="ul-data" style="display: none;" >
+              @foreach($posisi as $pos)
+              <li id="{{$pos->id}}" title="{{$pos->posisi}}">
+                @foreach($pos->anggota as $anggota)
+                  {{$anggota->nama}}
+                @endforeach
+                @if(count($pos->anak) != 0)
+                     <ul>
+                      @foreach($pos->anak as $anak)
+                        <li id="{{$anak->id}}" title="{{$anak->posisi}}">
+                          @foreach($anak->anggota as $key1=> $anggota1)
+                            {{$anggota1->nama}},
+                          @endforeach
+                          @if(count($anak->anak) != 0)
+                              <ul>
+                                @foreach($anak->anak as $anak2)
+                                    <li id="{{$anak->id}}" title="{{$anak2->posisi}}">
+                                    @foreach($anak2->anggota as $anggota2)
+                                        {{$anggota2->nama}},
+                                    @endforeach
+                                    @if(count($anak2->anak) != 0)
+                                      <ul>
+                                        @foreach($anak2->anak as $anak3)
+                                          <li id="{{$anak3->id}}" title="{{$anak3->posisi}}" >
+                                            @foreach($anak3->anggota as $anggota3)
+                                              {{$anggota3->nama}},                    
+                                            @endforeach
+                                            @if(count($anak3->anak) != 0)
+                                              <ul>
+                                                @foreach($anak3->anak as $anak4)
+                                                  <li id="{{$anak4->id}}" title="{{$anak4->posisi}}">
+                                                @endforeach
+                                              </ul>
+                                            @else
+                                          </li>
                                             @endif
                                         @endforeach
                                       </ul>
@@ -160,11 +216,35 @@
     //   'nodeContent': 'title'
     // });
     var nodeTemplate = function(data) {
-      console.log(data);
-      return `
+      var child = data.children;
+      if(child){
+        console.log(data.content);
+        $.each(data.children, function (index, child) {
+          child.parentId = data.id;
+          console.log(data);
+        })
+      }
+      var anggota = data.content;
+      anggota = anggota.split(',');
+      console.log(anggota);
+       $.each(anggota, function (index, child) {
+          if(child == ''){
+            anggota.splice(index,1);
+          }
+        })
+       console.log(anggota);
+        return `
         <div class="title  level-${data.level}">${data.title}</div>
-          <div class="content" style="white-space:pre-line;">${data.content}</div>
+          <div class="content ss">`+
+          $.each(anggota, function (index, child) {
+            child+'<br>'
+          })+`
+          </div>
       `;
+      // return `
+      //   <div class="title  level-${data.level}">${data.title}</div>
+      //     <div class="content" style="white-space:pre-line;">${data.content}<br></div>
+      // `;
     };
 
     $('#chart-container').orgchart({
