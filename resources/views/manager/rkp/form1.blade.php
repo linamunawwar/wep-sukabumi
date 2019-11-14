9d5484d1edf01c294576a2a805c52f55fa15fb7f
DESKTOP-NRTLCSN\Asus <nuramalina2911.na@gmail.com>
2019-11-14 08:49:41 +0700

    dispo monitor, form 1 rkp

---
+++ resources/views/manager/rkp/form1.blade.php
@@ -0,0 +1,163 @@
+<table>
+  <tr>
+    <td></td>
+    <td></td>
+    <th style="width: 6;"></th>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <th></th>
+    <th colspan="7"><b style="font-weight: 3;">PT. WASKITA KARYA (Persero) Tbk</b></th>
+    <th></th>
+    <th></th>
+    <th style="border: 1px solid #000000 width:25;" colspan="3">Form. WK-SDM-03-01</th>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th style="border: 1px solid #000000 width:25;" colspan="3"><b>Edisi : 3</b></th>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td>PUSAT</td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td><b>V</b></td>    
+    <td>Unit Kerja/Bisnis/Proyek</td>
+    <td>: Proyek Jalan Tol Becakayu Seksi 2A Ujung</td>
+  </tr>
+</table>
+
+<table class="table table-striped" style="text-align: center;">
+  <tr>
+    <td></td>
+    <td></td>
+    <td colspan="14" style="text-align: center;"><h4><b>RENCANA KEBUTUHAN PEGAWAI</b></h4></td>
+  </tr>
+</table>
+<table class="table table-striped">
+  <tr class="thead-light" >
+    <td></td>
+    <td></td>
+    <td style="border: 1px solid #000000;" rowspan="3" align="center">No.</th>
+    <td style="border: 1px solid #000000; width: 27;" rowspan="3" align="center">Unit Kerja</th>
+    <td style="border: 1px solid #000000; width: 27;" rowspan="3" align="center">Kebutuhan</th>
+    <td style="border: 1px solid #000000; width: 12;" rowspan="3" align="center">Tersedia</th>
+    <td style="border: 1px solid #000000; width: 12;" rowspan="3" align="center">Kurang / Lebih</th>
+    <td style="border: 1px solid #000000; width: 15;"  align="center" colspan="4">Pemenuhan</th>
+    <td style="border: 1px solid #000000; width: 12;" rowspan="3" align="center">Keterangan</th>
+  </tr>
+  <tr class="thead-light" style="text-align: center;">
+    <td></td>
+    <td></td>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <th></th>
+    <td style="border: 1px solid #000000;" colspan="4" >Promosi/Mutasi</td>
+    <td ></td>
+  </tr>
+  <tr class="thead-light" style="text-align: center;">
+    <td></td>
+    <td></td>
+    <td ></td>
+    <td ></td>
+    <td ></td>
+    <td ></td>
+    <td ></td>
+    <td style="border: 1px solid #000000;">Masuk</td>
+    <td style="border: 1px solid #000000;">Keluar</td>
+    <td style="border: 1px solid #000000;">Jumlah</td>
+    <td style="border: 1px solid #000000;">Rekrut</td>
+    <td></td>
+  </tr>
+  <?php $i =1; ?>
+  @foreach($dt_rkp as $data)
+    <tr>
+      <td></td>
+      <td></td>
+      <td style="border: 1px solid #000000;">{{$i++}}</td>
+      <td style="border: 1px solid #000000;" >{{$data->unit_kerja}}</td>
+      <td style="border: 1px solid #000000;">{{$data->kebutuhan}}</td>
+      <td style="border: 1px solid #000000;">{{$data->tersedia}}</td>
+      <td style="border: 1px solid #000000;">{{$data->kurang_lebih}}</td>
+      <td style="border: 1px solid #000000;">{{$data->masuk}}</td>
+      <td style="border: 1px solid #000000;">{{$data->keluar}}</td>
+      <td style="border: 1px solid #000000;">{{$data->jumlah}}</td>
+      <td style="border: 1px solid #000000;">{{$data->rekrut}}</td>
+      <td style="border: 1px solid #000000;"></td>
+    </tr>
+  @endforeach
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    @if($rkp->is_verif_pm ==1)
+      <td>Bekasi, {{formatTanggalPanjang($rkp->tanggal)}}</td>
+    @else
+      <td>Bekasi, </td>
+    @endif
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td align="center">Menyetujui,</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td>yang membuat,</td>
+  </tr>
+  <tr></tr>
+  <tr></tr>
+  <tr></tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td align="center">Sudiarso</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td>Deo Panggabean</td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td align="center">Site Administation Manager</td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td align="center" colspan="3">Site Adminitration Staff</td>
+  </tr>
+</table>
+  
