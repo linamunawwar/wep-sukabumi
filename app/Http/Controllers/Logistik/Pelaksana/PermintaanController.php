3212fba980a55921e211470e2c6cfbbcb417221b
DESKTOP-NRTLCSN\Asus <nuramalina2911.na@gmail.com>
2019-12-02 12:03:40 +0700

    controller permintaan

---
+++ app/Http/Controllers/Logistik/Pelaksana/PermintaanController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Logistik\Pelaksana;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PermintaanController extends Controller
+{
+    //
+}
