4eb7a0f98a9899986e79e20f7706cdd9e59d70b6
fanaproduction01 <fanaproduction01@gmail.com>
2019-12-09 18:57:49 +0700

    Error Download Permintaan

--- resources/views/logistik/admin/permintaan/index.blade.php
+++ resources/views/logistik/admin/permintaan/index.blade.php
@@ -47,12 +47,12 @@
 									<td>{{ $no }}</td>
 									<td>{{ $permintaan->kode_permintaan }}</td>
 									<td>{{ $permintaan->tanggal }}</td>
-									<td style="color:{{ $status['color'] }};">{{ $status['text'] }}</td>
+									<td style="color:{{ $cekStatus['color'] }};">{{ $cekStatus['text'] }}</td>
 									<td style="text-align:center;">
 										<a class="btn btn-default btn-xs" style="background-color:#FF9800; color:#FFFFFF; padding:0.5em 0.7em 0.5em 0.7em;" href="{{url('Logistik/admin/permintaan/detail/'.$permintaan->id.'')}}"><i class="fa fa-th-list" style="font-size:15px;"></i>  </a>
 										<a class="btn btn-default btn-xs" style="background-color:#1AAD19; color:#FFFFFF; padding:0.5em 0.7em 0.5em 0.7em;" href="{{url('Logistik/admin/permintaan/edit/'.$permintaan->id.'')}}"><i class="fa fa-pencil" style="font-size:15px;"></i>  </a>
 										<button data-toggle="modal"  id_permintaan='{{$permintaan->id}}' data-target="#DeleteModal" class="btn btn-danger btn-xs" style="background-color:#D63031; color:#FFFFFF; padding:0.5em 0.7em 0.5em 0.7em;" id="modal-delete" onclick='deleteData("{{$permintaan->id}}")'><i class="fa fa-trash" style="font-size:15px;"></i></button>
-										<a class="btn btn-default btn-xs" style="background-color:#0984E3; color:#FFFFFF; padding:0.5em 0.7em 0.5em 0.7em;" href="{{url('Logistik/admin/permintaan/download/'.$permintaan->id.'')}}"><i class="fa fa-download" style="font-size:15px;"></i>  </a>
+										<a class="btn btn-default btn-xs" style="background-color:#0984E3; color:#FFFFFF; padding:0.5em 0.7em 0.5em 0.7em;" href="{{url('Logistik/admin/permintaan/unduh/'.$permintaan->id.'')}}"><i class="fa fa-download" style="font-size:15px;"></i>  </a>
 									</td>
 									</tr>
 								@endforeach							
