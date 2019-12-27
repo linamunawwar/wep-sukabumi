0f4df42aea55d88422d58adcc293b14b147923f5
fanaproduction01 <fanaproduction01@gmail.com>
2019-12-27 11:33:23 +0700

    Done Laporan Evaluasi Mingguan Pengadaan Bahan

--- resources/views/logistik/admin/log05/index.blade.php
+++ resources/views/logistik/admin/log05/index.blade.php
@@ -30,7 +30,7 @@
 			<div class="col-md-12 col-sm-12 col-xs-12">
 				<div class="x_panel">
 					<div class="x_title">
-						<h2>Evaluasi Pemakaian Material (Log-06) </h2>
+						<h2>Buku Harian Gudang (Log-05) </h2>
 						<ul class="nav navbar-right panel_toolbox">
 						</ul>
 						<div class="clearfix"></div>
