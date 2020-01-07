b5d0ca96472eaa4762ed388c13f9fafa8baa79fd
fanaproduction01 <fanaproduction01@gmail.com>
2020-01-07 16:19:35 +0700

    Rev Log 03

--- resources/views/logistik/admin/log03/index.blade.php
+++ resources/views/logistik/admin/log03/index.blade.php
@@ -41,15 +41,23 @@
                             <div id="SearchForm">
 								<div class="row">
 									<div class="col-md-12"  style="padding: 0; margin: 0;">
-											<div class="form-group">
-												<label style="line-height:20px;" class="control-label col-md-2 col-sm-2 col-xs-2" for="nama">Kode Permintaan ( BPM )</label>
-												<div class="col-md-3 col-sm-3 col-xs-3">
-													<div class='input-group'>
-														<input type='text' value='' name='kodePermintaan' class='form-control' required="required" placeholder="Kode Permintaan" />
-													</div>                                     
-												</div>
-											</div>   
-										<div class="form-group" style="margin-top:-6em;">	
+										<div class="form-group">
+											<label style="line-height:20px;" class="control-label col-md-2 col-sm-2 col-xs-2" for="nama">Tanggal Mulai</label>
+											<div class="col-md-3 col-sm-3 col-xs-3">
+                                                <div class='input-group date' id='datepicker1' class="datepicker">
+                                                    <span class='input-group-addon'><span class='glyphicon glyphicon-calendar'></span></span>
+                                                    <input type='text' value='' name='tanggal_mulai' class='form-control' required="required" placeholder="dd-mm-yyyy" />
+                                                </div>                                     
+											</div>
+										</div>
+										<div class="form-group" style="margin-top:-6em;">
+											<label style="line-height:20px;" class="control-label col-md-2 col-sm-2 col-xs-2" for="nama">Tanggal Selesai</label>
+											<div class="col-md-3 col-sm-3 col-xs-3">
+                                                <div class='input-group date' id='datepicker2' class="datepicker">
+                                                    <span class='input-group-addon'><span class='glyphicon glyphicon-calendar'></span></span>
+                                                    <input type='text' value='' name='tanggal_selesai' class='form-control' required="required" placeholder="dd-mm-yyyy" />
+                                                </div>                                      
+                                            </div>	
                                             <div class="col-md-1">
                                                 <button class="btn btn-primary pull-right" id="search">Search</button>
                                             </div>
@@ -65,3 +73,16 @@
     </div>
 	<!-- /page content -->
 @endsection
+@push('scripts')
+  <script type="text/javascript">
+  	$('#datepicker1').datepicker({
+        format: 'dd-mm-yyyy',
+        autoclose: true
+	});
+
+	$('#datepicker2').datepicker({
+        format: 'dd-mm-yyyy',
+        autoclose: true
+	});
+  </script>
+ @endpush
