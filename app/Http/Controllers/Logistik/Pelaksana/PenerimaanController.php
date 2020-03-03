cf39412a3d217f4373f5904e62b92cb35a8385e3
linamunawwar <nuramalina2911.na@gmail.com>
2020-03-03 10:38:58 +0700

    hilangkan approval PM di penerimaan, notif saat ada penerimaan baru

--- app/Http/Controllers/Logistik/Pelaksana/PenerimaanController.php
+++ app/Http/Controllers/Logistik/Pelaksana/PenerimaanController.php
@@ -24,24 +24,30 @@ class PenerimaanController extends Controller
                 if ($penerimaan->is_splem == null) {
                     $penerimaan->color = "#D63031";
                     $penerimaan->text = "Proses Pengecekan";
-                } elseif ($penerimaan->is_slem == 0) {
+                } elseif ($penerimaan->is_splem == 0) {
                     $penerimaan->color = "#D63031";
                     $penerimaan->text = "Rejected By SPLEM";
                 }
-            } elseif ($penerimaan->is_pm != 1) {
-                if ($penerimaan->is_pm == null) {
-                    $penerimaan->color = "#74B9FF";
-                    $penerimaan->text = "Accepted By SPLEM";
-                } elseif ($penerimaan->is_pm == 0) {
-                    $penerimaan->color = "#D63031";
-                    $penerimaan->text = "Rejected By PM";
-                }
-            } elseif ($penerimaan->is_pm == 1) {
+            } elseif ($penerimaan->is_splem == 1) {
                 $penerimaan->color = "#74B9FF";
-                $penerimaan->text = "Accepted By PM";
+                $penerimaan->text = "Accepted By SPLEM";
             }
         }
         return view('logistik.pelaksana.penerimaan.index', ['penerimaans' => $penerimaans]);
+    }
+
+    public function getDetailByPenerimaanId($id)
+    {
+        $details = LogDetailPenerimaanMaterial::where(['penerimaan_id' => $id, 'soft_delete' => 0])->get();
+        $penerimaan = LogPenerimaanMaterial::find($id);
+        $permintaan = LogPermintaanMaterial::where('kode_permintaan',$penerimaan->kode_permintaan)->first();
+        if(\Auth::user()->id == $permintaan->user_id){
+            $toUpdatePenerimaan['is_new'] = 0;
+
+            $updatePenerimaan = LogPenerimaanMaterial::where('id',$id)->update($toUpdatePenerimaan);
+        }
+
+        return view('logistik.pelaksana.penerimaan.detail', ['details' => $details]);
     }
 
 }
--- app/helpers.php
+++ app/helpers.php
@@ -34,6 +34,18 @@ function notif_permintaan_disetujui()
     return $permintaan_disetujui;
 }
 
+function notif_penerimaan_baru()
+{
+    $user_id = \Auth::user()->id;
+    $baru = LogPenerimaanMaterial::where('soft_delete',0)
+            ->where('is_new',1)
+            ->whereHas('permintaan',function ($q) use($user_id){
+              $q->where('user_id', $user_id);
+            })
+            ->get();
+    return $baru;
+}
+
 function formatTanggalPanjang($tanggal) {
     $aBulan = array(1=> "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
     list($thn,$bln,$tgl)=explode("-",$tanggal);
